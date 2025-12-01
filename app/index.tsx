8686b2e3990871842bd26176129ea52093fbf926
Đỗ Thành Nhân <do.thanh.nhan2@rainscales.com.vn>
2025-12-01 13:55:12 +0700

    init project

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="bg-primary flex-1">
+      <Text className="text-3xl text-white">
+        Edit app/index.tsx to edit this screen.
+      </Text>
+
+      <Text>ABC</Text>
+    </View>
+  );
+}

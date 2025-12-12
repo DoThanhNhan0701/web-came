67ac4bf52b1c03d6249b51d89b95071183a20bb2
Đỗ Thành Nhân <do.thanh.nhan2@rainscales.com.vn>
2025-12-12 14:41:31 +0700

    feat: Update app configuration; add EAS build settings and enhance API URL handling

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,4 +1,5 @@
-export const API_URL = process.env.EXPO_PUBLIC_API_URL;
+export const API_URL =
+  process.env.EXPO_PUBLIC_API_URL ?? "https://take-a-photo.aiminds.io.vn";
 
 export const ACCESS_TOKEN = "_access_token";
 export const REFRESH_TOKEN = "_refresh_token";
